5a827f4d46e1d4b5309dab0b2da83e99199a9c33
Giles Dang <gilesdang@augmentum.com.cn>
2015-01-26 18:07:43 +0800

    first commit about splash activity

---
+++ HappyForU/app/src/main/java/com/augmentum/giles/happyforu/WelcomeActivity.java
@@ -0,0 +1,90 @@
+package com.augmentum.giles.happyforu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.augmentum.giles.happyforu.ui.LoginActivity;
+
+
+public class WelcomeActivity extends Activity {
+
+    private final static int MSG_WAIT_CODE = 520;
+
+    private Thread mThreadWait = new Thread()
+    {
+        @Override
+        public void run() {
+            super.run();
+            try {
+                Thread.sleep(6000);
+                Message msg = new Message();
+                msg.what = MSG_WAIT_CODE;
+                mHandler.sendMessage(msg);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    private Handler mHandler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == MSG_WAIT_CODE)
+            {
+                // TODO go to home activity.
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    };
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mThreadWait.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mThreadWait.interrupt();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_welcome, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ HappyForU/app/src/main/java/com/augmentum/giles/happyforu/ui/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.augmentum.giles.happyforu.ui;
+
+import android.app.Activity;
+
+/**
+ * Created by user on 15-1-26.
+ */
+public class LoginActivity extends Activity{
+}
